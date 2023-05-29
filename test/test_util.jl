15376922e0b3a59cd36c75d9b88103780e838fe4
Curt Da Silva <curt.dasilva@gmail.com>
2023-05-29 11:40:25 -0700

    test: Added compatibility macro for testing in 1.6

--- test/test_util.jl
+++ test/test_util.jl
@@ -1,3 +1,12 @@
+macro test_throws_compat(ExceptionType, message, expr)
+    output = Expr(:block)
+
+    push!(output.args, :(@test_throws $ExceptionType $expr))
+    if VERSION ≥ v"1.7"
+        push!(output.args, :(@test_throws $message $expr))
+    end
+    return output |> esc
+end
 @testset "Macro utilities" begin 
     @test TestingUtilities.unwrap_if_quotenode(:x) == :x 
     @test TestingUtilities.unwrap_if_quotenode(QuoteNode(:x)) == :x 
@@ -6,8 +15,9 @@
         ex = [:(a=1), :(d=A.b), :(c=def), :(b), :(e=:e)]
         kwargs = TestingUtilities.parse_kwarg_expr(ex...)
         @test kwargs == Dict{Symbol, Any}(:a => (; position=1, value=1), :b => (; position=4, value=:b), :c => (; position=3, value=:def), :d => (; position=2, value=:(A.b)), :e => (; position=5, value=QuoteNode(:e)))
-        @test_throws "Argument f(a) must be a Symbol, QuoteNode, or Assignment expression" TestingUtilities.parse_kwarg_expr(:(f(a)))
-        @test_throws "In expression f(a) = b, key f(a) must be a QuoteNode or a Symbol, got typeof(key) = Expr" TestingUtilities.parse_kwarg_expr(Expr(:(=), :(f(a)), :b))
+        @test_throws_compat ErrorException "Argument f(a) must be a Symbol, QuoteNode, or Assignment expression" TestingUtilities.parse_kwarg_expr(:(f(a)))
+       
+        @test_throws_compat ErrorException "In expression f(a) = b, key f(a) must be a QuoteNode or a Symbol, got typeof(key) = Expr" TestingUtilities.parse_kwarg_expr(Expr(:(=), :(f(a)), :b))
     
         @test TestingUtilities.fetch_kwarg_expr(kwargs; key=:a, expected_type=[Int,QuoteNode]) == 1
         @test TestingUtilities.fetch_kwarg_expr(kwargs; arg_position=1, expected_type=[Int,QuoteNode]) == 1
@@ -22,8 +32,8 @@
         @test_throws ArgumentError TestingUtilities.fetch_kwarg_expr(kwargs; arg_position=10, expected_type=[Int,Symbol,QuoteNode])
         @test_throws ArgumentError TestingUtilities.fetch_kwarg_expr(kwargs; key=:f, expected_type=[Int,QuoteNode]) 
         @test TestingUtilities.fetch_kwarg_expr(kwargs; key=:f, default_value=10, expected_type=[Int,QuoteNode]) == 10
-        @test_throws "ArgumentError: key (= f) = value (= abc) must be one of DataType[Int64, QuoteNode], got typeof(value) = String" TestingUtilities.fetch_kwarg_expr(kwargs; key=:f, default_value="abc", expected_type=[Int,QuoteNode])
-        @test_throws "ArgumentError: key (= f) not found in keys(kwargs)" TestingUtilities.fetch_kwarg_expr(kwargs; key=:f, default_value=nothing, expected_type=[Int,QuoteNode])
+        @test_throws_compat ArgumentError "key (= f) = value (= abc) must be one of DataType[Int64, QuoteNode], got typeof(value) = String" TestingUtilities.fetch_kwarg_expr(kwargs; key=:f, default_value="abc", expected_type=[Int,QuoteNode])
+        @test_throws_compat ArgumentError "key (= f) not found in keys(kwargs)" TestingUtilities.fetch_kwarg_expr(kwargs; key=:f, default_value=nothing, expected_type=[Int,QuoteNode])
         @test TestingUtilities.fetch_kwarg_expr(kwargs; key=:f, default_value=nothing, expected_type=[Int,QuoteNode, Nothing]) |> isnothing
     end
 end
