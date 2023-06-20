e6879519e25f3b50c1da68e1cce8ca030daed02d
Curt Da Silva <curt.dasilva@gmail.com>
2023-06-20 15:05:27 -0700

    fix: Only run Aqua tests on Julia 1.9+

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using TestingUtilities
-using Aqua
-Aqua.test_all(TestingUtilities)
+
+if VERSION ≥ v"1.9"
+    using Aqua
+    Aqua.test_all(TestingUtilities)
+end
 
 include("TestTestingUtilities.jl")
